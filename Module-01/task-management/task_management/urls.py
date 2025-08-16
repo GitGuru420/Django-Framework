17635c405a30f6af7ab019281d844e18042f743d
GitGuru420 <raisulislam1892@gmail.com>
2025-08-16 17:03:08 +0600

    Define urls and create function based views

--- Module-01/task-management/task_management/urls.py
+++ Module-01/task-management/task_management/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path
+from tasks.views import home, contact
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/', home),
+    # path('', home),
+    path('contact/', contact),
 ]
--- Module-01/task-management/tasks/views.py
+++ Module-01/task-management/tasks/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+    return HttpResponse("<h1 style='color: purple'>Welcome to the Task Management System</h1>")
+
+def contact(request):
+    return HttpResponse("<h1 style='color: red'>This is Contact Page</h1>")
