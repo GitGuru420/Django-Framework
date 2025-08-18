dde0f3c247af3db021b9a4a05c8896a08ba7bb12
GitGuru420 <raisulislam1892@gmail.com>
2025-08-18 11:12:15 +0600

    Exploring django templates tags

--- Module-04/task-management/tasks/views.py
+++ Module-04/task-management/tasks/views.py
@@ -9,8 +9,13 @@ def user_dashboard(request):
     return render(request, "dashboard/user-dashboard.html")
 
 def test(request):
+    names = ['Mahmud', 'Ahmed', 'John']
+    count = 0
+    for name in names:
+        count += 1
     context = {
         'names': ['Mahmud', 'Ahmed', 'John'],
-        'age': 23
+        'age': 23,
+        'count': count
     }
     return render(request, "test.html", context)
