698e3996440f666f5eba8bfa4ee78ea1ac78a088
GitGuru420 <raisulislam1892@gmail.com>
2025-08-20 19:50:58 +0600

    Configuring PostgreSQL

---
+++ Module-05/task-management/tasks/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# app folder templates
+def manager_dashboard(request):
+    return render(request, "dashboard/manager-dashboard.html")
+
+def user_dashboard(request):
+    return render(request, "dashboard/user-dashboard.html")
+
+def test(request):
+    names = ['Mahmud', 'Ahmed', 'John']
+    count = 0
+    for name in names:
+        count += 1
+    context = {
+        'names': ['Mahmud', 'Ahmed', 'John'],
+        'age': 23,
+        'count': count
+    }
+    return render(request, "test.html", context)
