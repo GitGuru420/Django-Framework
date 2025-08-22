4665db0355f54c91eef9dcfec0915eeba9710696
GitGuru420 <raisulislam1892@gmail.com>
2025-08-22 09:51:56 +0600

    Django Model Form

--- Module-06/task-management/tasks/forms.py
+++ Module-06/task-management/tasks/forms.py
@@ -1,5 +1,7 @@
 from django import forms
+from tasks.models import Task
 
+# Django Form
 class TaskForm(forms.Form):
     title = forms.CharField(max_length=100, label="Task Title")
     description = forms.CharField(widget=forms.Textarea, label="Task Description")
@@ -14,3 +16,17 @@ class TaskForm(forms.Form):
         super().__init__(*args, **kwargs)
         # print(self.fields)
         self.fields['assigned_to'].choices = [(emp.id, emp.name) for emp in employees]
+        
+
+# Django Model Form
+class TaskModelForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        # fields = '__all__' # all fields
+        fields = ['title', 'description', 'due_date', 'assigned_to']
+        # exclude = ['project', 'is_completed', 'created_at', 'updated_at']
+        
+        widgets = {
+            'due_date': forms.SelectDateWidget,
+            'assigned_to': forms.CheckboxSelectMultiple
+        }
--- Module-06/task-management/tasks/views.py
+++ Module-06/task-management/tasks/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from tasks.forms import TaskForm
+from tasks.forms import TaskForm, TaskModelForm
 from tasks.models import Employee, Task
 
 # app folder templates
@@ -25,28 +25,35 @@ def test(request):
 # form : post
 def create_task(request):
     employees = Employee.objects.all()
-    form = TaskForm(employees=employees)    # For GET
+    form = TaskModelForm()    # For GET
     
     # For POST
     if request.method == "POST":
-        form = TaskForm(request.POST, employees=employees)
+        form = TaskModelForm(request.POST)
         # print(form)
         if form.is_valid():
-            # print(form.cleaned_data)
-            data = form.cleaned_data
-            title = data.get('title')
-            description = data.get('description')
-            due_date = data.get('due_date')
-            assigned_to = data.get('assigned_to')
+            """ For Django Form Data """
+            # # print(form.cleaned_data)
+            # data = form.cleaned_data
+            # title = data.get('title')
+            # description = data.get('description')
+            # due_date = data.get('due_date')
+            # assigned_to = data.get('assigned_to')
             
-            task = Task.objects.create(title=title, description=description, due_date=due_date)
+            # task = Task.objects.create(title=title, description=description, due_date=due_date)
             
-            # assigne employee to tasks
-            for emp_id in assigned_to:
-                employee = Employee.objects.get(id=emp_id)
-                task.assigned_to.add(employee)
+            # # assigne employee to tasks
+            # for emp_id in assigned_to:
+            #     employee = Employee.objects.get(id=emp_id)
+            #     task.assigned_to.add(employee)
             
-            return HttpResponse("Task Added Successfully")
+            # return HttpResponse("Task Added Successfully")
+            
+            
+            """ For Model Form Data """
+            form.save()
+            
+            return render(request, 'task_form.html', {"form": form, "message": "task added successfully"})
             
     context = {"form": form}
     return render(request, "task_form.html", context)
