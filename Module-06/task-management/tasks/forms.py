6d1be8046d677a97381e7e36355db1b9ba434eaf
GitGuru420 <raisulislam1892@gmail.com>
2025-08-22 11:43:12 +0600

    Custom widget in django model form

--- Module-06/task-management/tasks/forms.py
+++ Module-06/task-management/tasks/forms.py
@@ -28,17 +28,18 @@ class TaskModelForm(forms.ModelForm):
         
         widgets = {
             'title': forms.TextInput(attrs= {
-                'class': "border-2 border-gray-300 w-full rounded-lg shadow-sm",
-                'placeholder': "Enter task title"
+                'class': "border-2 border-gray-300 w-full p-3 rounded-lg shadow-sm focus:outline-none focus:border-rose-500 focus:ring-rose-500",
+                'placeholder': "Enter a descriptive task title"
             }),
             'description': forms.Textarea(attrs= {
-                'class': "border-2 border-gray-300 w-full rounded-lg shadow-sm",
-                'placeholder': "Describe the task"
+                'class': "border-2 border-gray-300 w-full p-3 rounded-lg shadow-sm resize-none focus:outline-none focus:border-rose-500 focus:ring-rose-500",
+                'placeholder': "Provide detailed task information",
+                'rows': 5,
             }),
             'due_date': forms.SelectDateWidget(attrs= {
-                'class': "border-2 border-gray-300 rounded-lg shadow-sm"
+                'class': "border-2 border-gray-300 p-2 rounded-lg shadow-sm focus:outline-none focus:border-rose-500 focus:ring-rose-500"
             }),
             'assigned_to': forms.CheckboxSelectMultiple(attrs= {
-                'class': "border-2 border-gray-300 w-full rounded-lg shadow-sm"
+                'class': "space-y-2"
             })
         }
