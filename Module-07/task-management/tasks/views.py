13ec2e76d46f7517bb2506ece6261e202142047d
GitGuru420 <raisulislam1892@gmail.com>
2025-08-24 09:03:05 +0600

    Aggregations

--- Module-07/task-management/tasks/views.py
+++ Module-07/task-management/tasks/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from tasks.forms import TaskForm, TaskModelForm
 from tasks.models import Employee, Task, TaskDetail, Project
 from datetime import date
-from django.db.models import Q
+from django.db.models import Q, Count, Max, Min, Avg
 
 # app folder templates
 def manager_dashboard(request):
@@ -45,12 +45,8 @@ def create_task(request):
 
 
 def view_task(request):
-    """select related (foreignkey, onetoonefieild)"""
-    # tasks = Task.objects.select_related('project').all()
+    # task_count = Task.objects.aggregate(num_task=Count('id'))
     
-    """prefetch related(reverse foreign key, many to many)"""
-    # tasks = Project.objects.prefetch_related('task_set').all()
-    
-    """many to many"""
-    tasks = Task.objects.prefetch_related('assigned_to').all()
-    return render(request, "show_task.html", {"tasks": tasks})
+    # projects = Project.objects.annotate(num_task=Count('task'))
+    projects = Project.objects.annotate(num_task=Count('task')).order_by('num_task')
+    return render(request, "show_task.html", {"projects": projects})
