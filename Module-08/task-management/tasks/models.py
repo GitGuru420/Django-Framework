f9dee2fccfd0c9e3b3cea8048d2b41f916ae664d
GitGuru420 <raisulislam1892@gmail.com>
2025-08-24 12:13:44 +0600

    Module-08

---
+++ Module-08/task-management/tasks/models.py
@@ -0,0 +1,71 @@
+from django.db import models
+
+class Task(models.Model):
+    STATUS_CHOICES = [
+        ('PENDING', ('Pending')),
+        ('IN_PROGRESS', ('In Progress')),
+        ('COMPLETED', ('Completed'))
+    ]
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    due_date = models.DateField()
+    status = models.CharField(max_length=15, choices=STATUS_CHOICES, default="PENDING")
+    is_completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # many to one
+    project = models.ForeignKey(
+        'Project', 
+        on_delete=models.CASCADE,
+        default=1
+    )
+    
+    # many to many
+    assigned_to = models.ManyToManyField(
+        'Employee',
+        related_name='tasks'
+    )
+    
+    def __str__(self):
+        return self.title
+    
+# one to one
+class TaskDetail(models.Model):
+    HIGH = 'H'
+    MEDIUM = 'M'
+    LOW = 'L'
+    PRIORITY_OPTIONS = (
+        (HIGH, 'High'),
+        (MEDIUM, 'Medium'),
+        (LOW, 'Low')
+    )
+    assigned_to = models.CharField(max_length=100)
+    priority = models.CharField(max_length=1, choices=PRIORITY_OPTIONS, default=LOW)
+    task = models.OneToOneField(
+        Task, 
+        on_delete=models.CASCADE,
+        related_name='details'
+    )
+    notes = models.TextField(blank=True, null=True)
+    
+    def __str__(self):
+        return f"Details for task {self.task.title}"
+    
+# many to one
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    start_date = models.DateField()
+    
+    def __str__(self):
+        return self.name
+    
+    
+# many to many
+class Employee(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    
+    def __str__(self):
+        return self.name
---
+++ Module-08/task-management/tasks/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from tasks.forms import TaskForm, TaskModelForm
+from tasks.models import Employee, Task, TaskDetail, Project
+from datetime import date
+from django.db.models import Q, Count, Max, Min, Avg
+
+# app folder templates
+def manager_dashboard(request):
+    return render(request, "dashboard/manager-dashboard.html")
+
+def user_dashboard(request):
+    return render(request, "dashboard/user-dashboard.html")
+
+def test(request):
+    names = ['Mahmud', 'Ahmed', 'John']
+    count = 0
+    for name in names:
+        count += 1
+    context = {
+        'names': ['Mahmud', 'Ahmed', 'John'],
+        'age': 23,
+        'count': count
+    }
+    return render(request, "test.html", context)
+
+# form : post
+def create_task(request):
+    employees = Employee.objects.all()
+    form = TaskModelForm()    # For GET
+    
+    # For POST
+    if request.method == "POST":
+        form = TaskModelForm(request.POST)
+        # print(form)
+        if form.is_valid():
+            
+            """ For Model Form Data """
+            form.save()
+            
+            return render(request, 'task_form.html', {"form": form, "message": "task added successfully"})
+            
+    context = {"form": form}
+    return render(request, "task_form.html", context)
+
+
+def view_task(request):
+    # task_count = Task.objects.aggregate(num_task=Count('id'))
+    
+    # projects = Project.objects.annotate(num_task=Count('task'))
+    projects = Project.objects.annotate(num_task=Count('task')).order_by('num_task')
+    return render(request, "show_task.html", {"projects": projects})
