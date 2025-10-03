b400a209a40ef6fc8b3b820a0615bc4443ae0d8c
GitGuru420 <raisulislam1892@gmail.com>
2025-10-03 09:49:09 +0600

    Working with app urls

--- Practice/task-management-system/tasks/views.py
+++ Practice/task-management-system/tasks/views.py
@@ -6,3 +6,6 @@ def home(request):
 
 def contact(request):
     return HttpResponse("<h1>Welcome to the Task Management System Contact Page</h1>")
+
+def show_task(request):
+    return HttpResponse("<h1>This is our Task Page</h1>")
